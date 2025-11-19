b6c409b9b7961f6bc4f363c8a49bf31923ae56c2
JonasBondoc26 <>
2025-11-19 15:49:31 +0800

    Updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header"
 import { Skills } from "./skills/skills";
+import { Education } from "./education/education"
+import { Hobbies } from "./hobbies/hobbies"
 import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Skills, Footer],
+  imports: [RouterOutlet, Header, Skills, Education, Hobbies, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
